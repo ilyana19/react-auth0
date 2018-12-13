cdef549ef2111f8d4d63a06eaedea81ab4603166
ilyana19 <aries_2152@hotmail.com>
2018-12-13 17:43:41 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './App.css';
 import {Route} from 'react-router-dom';
+import Nav from './components/Nav'
 import Home from './components/Home'
 import Profile from './components/Profile'
 
@@ -8,10 +9,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome</h1>
+        <Nav />
 
-        <Route path="/" exact component={Home} />
-        <Route path="/profile" component={Profile} />
+        <div className="body">
+          <Route path="/" exact component={Home} />
+          <Route path="/profile" component={Profile} />
+        </div>
       </div>
     );
   }
