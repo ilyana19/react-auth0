54f7eeda47872ba5fff13b2b6a81a3a50cbd46c1
ilyana19 <aries_2152@hotmail.com>
2018-12-13 18:59:52 -0500

    setup login with auth0

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Route} from 'react-router-dom';
-import Nav from './components/Nav'
-import Home from './components/Home'
-import Profile from './components/Profile'
+import Auth from "./Auth/Auth";
+
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Callback from './components/Callback';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.auth = new Auth(this.props.history)
+  }
+
   render() {
     return (
       <div>
         <Nav />
 
         <div className="body">
-          <Route path="/" exact component={Home} />
+          <Route
+            path="/"
+            exact
+            render={props => <Home auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={props => <Callback auth={this.auth} {...props} />}
+          />
           <Route path="/profile" component={Profile} />
         </div>
       </div>
