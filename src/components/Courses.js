3706398f5493b48c345c3667b3e1d7b3e6210527
ilyana19 <aries_2152@hotmail.com>
2018-12-14 10:44:06 -0500

    added roles

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 class Courses extends Component {
   state = {
-    courses: []
+    courses: [],
+    admin: ""
   };
 
   componentDidMount() {
@@ -15,14 +16,28 @@ class Courses extends Component {
       })
       .then(response => this.setState({ courses: response.courses }))
       .catch(error => this.setState({ message: error.message }));
+
+    fetch("/admin", {
+      headers: { Authorization: `Bearer ${this.props.auth.getAccessToken()}` }
+    })
+      .then(response => {
+        if (response.ok) return response.json();
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => this.setState({ admin: response.message }))
+      .catch(error => this.setState({ message: error.message }));
   }
 
   render() {
     return (
-      <ul> {this.state.courses.map(course => {
-        return <li key={course.id}>{course.title}</li>
-      })}
-      </ul>
+      <div>
+        <h1>Message: {this.state.admin}</h1>
+
+        <ul> {this.state.courses.map(course => {
+          return <li key={course.id}>{course.title}</li>
+        })}
+        </ul>
+      </div>
     );
   }
 }
